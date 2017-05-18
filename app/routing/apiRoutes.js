3312638d916987f2afb2dd0276f7dd68e6733085
Alex Lovell <alexlovell@MacBook-Pro.local>
2017-05-17 21:11:12 -0400

    jquery

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,42 +14,43 @@ module.exports = function(app) {
     // when user submits a form this handles all the most and posts them inside the API along with matching
     // them to current friends
     app.post("/api/friends", function(req, res) {
-        var newFriend = {
-            name: "",
-            image: "",
-            matchDifference: 1000
-        }
-
-        var newData = req.body;
-        var newName = newData.name;
-        var newImage = newData.image;
-        var newScores = newData.scores;
+    	// takes new friends scores and puts them in an array that compares to current friends
+        var newFriendScore = req.body.scores;
+        var scoresArray = [];
+        var friendcount = 0;
+        var bestMatch = 0;
 
 
-        var totalDifference = 0;
 
-        console.log(newFriend);
+
+        console.log(newFriendScore);
 
         // need to loop through friends array to find each score
-        for (var i = 0; i < [friendInput].length - 1; i++) {
+        for (var i = 0; i < [friendInput].length; i++) {
             console.log(friendInput[i].name);
-            totalDifference = 0;
+
+            var totalDifference = 0;
 
             // loop through friends and users score and calculate absolute difference and push total to newData
-            for (var j = 0; j < 10; j++) {
-                totalDifference += Math.abs(parseInt(newScores[j]) - parseInt(friendInput[i].scores[j]));
-
-                if (totalDifference <= newFriend.friendDifference) {
-
-                    newFriend.name = friendInput[i].name;
-                    newFriend.photo = friendInput[i].photo;
-                    newFriend.matchDifference = totalDifference;
-                }
+            for (var j = 0; j < newFriendScore.length; j++) {
+                totalDifference += Math.abs(parseInt(friendInput[i].scores[j]) - parseInt(newFriendScore[j]));
+            }
+            // push new scores into array
+            scoresArray.push(totalDifference);
+        }
+        // loop through array and find the best match
+        for (var i = 0; i < scoresArray.length; i++) {
+            if (scoresArray[i] <= scoresArray[bestMatch]) {
+                bestMatch = i;
             }
         }
-        friendInput.push(newData);
+        // return the best match and put it into JSON formatting along with the new site
+        var bestFriend = friendInput[bestMatch];
 
-        res.json(newFriend);
+        res.json(bestFriend);
+        // push submission into friendlist array
+        friendInput.push(req.body);
+
     });
 
 
