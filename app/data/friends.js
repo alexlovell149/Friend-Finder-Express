de6d23f0c21b355a5bf420cab24f9274744f5b87
Alex Lovell <alexlovell@MacBook-Pro.local>
2017-05-17 22:28:14 -0400

    ok

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,24 +1,24 @@
 var friends = [{
     "name": "Monica",
     "photo": "https://scontent-dfw1-1.xx.fbcdn.net/t31.0-8/13116244_10107568960519320_7024834722285826624_o.jpg",
-    "scores": [5, 5, 2, 3, 1, 5, 2, 1, 3, 5]
+    "scores": ["5", "5", "2", "3", "1", "5", "2", "1", "3", "5"]
 
 }, {
     "name": "Dave",
     "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
 }, {
     "name": "Rose",
     "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [1, 2, 4, 3, 4, 1, 5, 3, 3, 2]
+    "scores": ["1", "2", "4", "3", "4", "1", "5", "3", "3", "2"]
 }, {
     "name": "Don",
     "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [2, 3, 1, 5, 3, 2, 5, 5, 2, 1]
+    "scores": ["2", "3", "1", "5", "3", "2", "5", "5", "2", "1"]
 }, {
     "name": "Kate",
     "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [5, 1, 4, 3, 2, 3, 3, 2, 2, 2]
+    "scores": ["5", "1", "4", "3", "2", "3", "3", "2", "2", "2"]
 }, {
     "name": "Ben",
     "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
