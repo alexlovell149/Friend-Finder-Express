a4ea8ffd50b169bcd3715451973de6f1ceeb65b1
Alex Lovell <alexlovell@MacBook-Pro.local>
2017-05-18 00:54:48 -0400

    math

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,47 +1,47 @@
 var friends = [{
     "name": "Monica",
-    "photo": "stock_woman5.jpg",
+    "photo": "http://versedonline.com/wp-content/uploads/2011/05/Mia.jpg",
     "scores": ["5", "5", "2", "3", "1", "5", "2", "1", "3", "5"]
 
 }, {
     "name": "Dave",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://economics.stanford.edu/sites/default/files/donaldson.jpg",
     "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
 }, {
     "name": "Rose",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://s-media-cache-ak0.pinimg.com/736x/8b/c3/c6/8bc3c68d4b47b89ebcb15fe4fcec9959.jpg",
     "scores": ["1", "2", "4", "3", "4", "1", "5", "3", "3", "2"]
 }, {
     "name": "Don",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "http://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2016/03/09/103455746-GettyImages-512791400.600x400.jpg?v=1457542554",
     "scores": ["2", "3", "1", "5", "3", "2", "5", "5", "2", "1"]
 }, {
     "name": "Kate",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "http://img.usmagazine.com/article-leads-vertical-300/1291319860_bio-kate-middleton-290.jpg",
     "scores": ["5", "1", "4", "3", "2", "3", "3", "2", "2", "2"]
 }, {
     "name": "Ben",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "http://ben.balter.com/assets/img/headshot.jpg",
     "scores": ["2", "4", "1", "2", "1", "5", "3", "1", "5", "5"]
 }, {
     "name": "Lisa",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://yt3.ggpht.com/-9bNdIDrLvWA/AAAAAAAAAAI/AAAAAAAAAAA/eyEzuxSoDzQ/s900-c-k-no-mo-rj-c0xffffff/photo.jpg",
     "scores": ["3", "4", "1", "3", "2", "3", "2", "5", "4", "3"]
 }, {
     "name": "Evan",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTU2NTg1OTQzMF5BMl5BanBnXkFtZTcwNjIyMjkyMg@@._V1_UY317_CR6,0,214,317_AL_.jpg",
     "scores": ["5", "3", "5", "2", "4", "2", "3", "4", "4", "3"]
 }, {
     "name": "Jason",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://vignette2.wikia.nocookie.net/bigbrother/images/8/88/Jason_BB3.jpg/revision/latest?cb=20121118114448",
     "scores": ["5", "3", "2", "3", "2", "4", "3", "1", "2", "3"]
 }, {
     "name": "Alyssa",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://pbs.twimg.com/profile_images/535478958645460992/IdeeceRq.jpeg",
     "scores": ["2", "4", "4", "3", "1", "4", "5", "2", "3", "4"]
 }, {
     "name": "Ken",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://peopledotcom.files.wordpress.com/2016/10/ken-bone.jpg?w=600",
     "scores": ["5", "1", "4", "3", "3", "4", "2", "2", "4", "3"]
 }];
 
