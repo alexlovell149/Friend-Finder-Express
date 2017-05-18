36e54a011a44f48ac572c3c308326f6cc6908414
Alex Lovell <alexlovell@MacBook-Pro.local>
2017-05-17 21:41:03 -0400

    debu

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-app.use("/", express.static(__dirname + 'app/public'));
 
 require('./app/routing/apiRoutes.js')(app); 
 require('./app/routing/htmlRoutes.js')(app);
