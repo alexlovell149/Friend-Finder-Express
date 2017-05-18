9526841e11c88ec58d5acde19449dcc0c8b190c1
Alex Lovell <alexlovell@MacBook-Pro.local>
2017-05-18 00:14:05 -0400

    math

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friends = [{
     "name": "Monica",
-    "photo": "https://scontent-dfw1-1.xx.fbcdn.net/t31.0-8/13116244_10107568960519320_7024834722285826624_o.jpg",
+    "photo": "stock_woman5.jpg",
     "scores": ["5", "5", "2", "3", "1", "5", "2", "1", "3", "5"]
 
 }, {
